394f8103c59d69388a9726afe6ae3dce87bd505a
Marcos Iglesias <markkos89@gmail.com>
2023-11-26 04:30:00 -0300

    feat: added borrowers/applications info table, post form mutation and getAll query

--- apps/jurisfund/src/pages/borrowerform.tsx
+++ apps/jurisfund/src/pages/borrowerform.tsx
@@ -1,26 +1,45 @@
+import { useRouter } from "next/router";
 import * as React from "react";
 
+import { api } from "@/utils/api";
+
 export default function BorrowerFormPage() {
-  const [message, setMessage] = React.useState("");
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [phoneNumber, setPhoneNumber] = React.useState("");
+  const [stateLiveIn, setStateLiveIn] = React.useState("");
 
-  // const postMessage = api.guestbook.postMessage.useMutation();
+  const [typeOfCase, setTypeOfCase] = React.useState(""); // options: Auto Accident - Workers' Compensation - Slip and Fall (Injury on Others' Property) - Wrongful Termination (Employment)
+  const [alreadyWorkingWithAttorney, setAlreadyWorkingWithAttorney] = React.useState("");
+  const [lawFirmName, setLawFirmName] = React.useState("");
+  const [lawyerName, setLawyerName] = React.useState("");
+  const [expectedSettlementAmount, setExpectedSettlementAmount] = React.useState("");
 
-  // const utils = api.useContext();
-  // const postMessage = api.guestbook.postMessage.useMutation({
-  //   onMutate: async (newEntry) => {
-  //     await utils.guestbook.getAll.cancel();
-  //     utils.guestbook.getAll.setData(undefined, (prevEntries) => {
-  //       if (prevEntries) {
-  //         return [newEntry, ...prevEntries];
-  //       } else {
-  //         return [newEntry];
-  //       }
-  //     });
-  //   },
-  //   onSettled: async () => {
-  //     await utils.guestbook.getAll.invalidate();
-  //   },
-  // });
+  const router = useRouter();
+
+  const postApplication = api.applications.add.useMutation({
+    // onMutate: async (newEntry) => {
+    //   await api.applications.getAll.invalidateQueries;
+    //   api.applications.getAll.setData(undefined, (prevEntries) => {
+    //     if (prevEntries) {
+    //       return [newEntry, ...prevEntries];
+    //     } else {
+    //       return [newEntry];
+    //     }
+    //   });
+    // },
+    // onSettled: async () => {
+    //   await api.applications.getAll.invalidate();
+    // },
+    onSuccess: async () => {
+      await router.push("/borrowerformsuccess");
+    },
+    onError(error, variables /*, context */) {
+      console.error({ error, variables });
+      alert("An error occurred while submitting the form.");
+    },
+  });
 
   return (
     <section className="relative  gap-2 ">
@@ -28,108 +47,280 @@ export default function BorrowerFormPage() {
         className="bg-orange-200-900 mx-60 flex flex-col gap-4 rounded-md border-2 border-orange-300 p-4"
         onSubmit={(event) => {
           event.preventDefault();
-          // postMessage.mutate({
-          //   name: session.user?.name as string,
-          //   message,
-          // });
-          setMessage("");
+          postApplication.mutate({
+            firstName,
+            lastName,
+            email,
+            phoneNumber,
+            stateLiveIn,
+            typeOfCase,
+            alreadyWorkingWithAttorney,
+            lawFirmName,
+            lawyerName,
+            expectedSettlementAmount,
+            walletAddress: "0xt3st",
+          });
+          setFirstName("");
+          setLastName("");
+          setEmail("");
+          setPhoneNumber("");
+          setStateLiveIn("");
+          setTypeOfCase("");
+          setAlreadyWorkingWithAttorney("");
+          setLawFirmName("");
+          setLawyerName("");
+          setExpectedSettlementAmount("");
         }}
       >
         <div className="flex flex-row content-center gap-4 text-center">
-          <label className="row w-[50%] text-black">Name</label>
+          <label className="row w-[50%] text-black">First Name</label>
           <input
             type="text"
-            className="row rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 focus:outline-none"
-            placeholder="Your message..."
+            className="row rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 text-yellow-100 focus:outline-none"
+            placeholder="Your First Name..."
             minLength={2}
             maxLength={100}
-            value={message}
+            value={firstName}
             onChange={(event) => {
-              setMessage(event.target.value);
+              setFirstName(event.target.value);
             }}
           />
         </div>
         <div className="flex flex-row content-center gap-4 text-center">
-          <label className="row w-[50%] text-black">Name</label>
+          <label className="row w-[50%] text-black">Last Name</label>
           <input
             type="text"
-            className="row rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 focus:outline-none"
-            placeholder="Your message..."
+            className="row rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 text-yellow-100 focus:outline-none"
+            placeholder="Your Last Name..."
             minLength={2}
             maxLength={100}
-            value={message}
+            value={lastName}
             onChange={(event) => {
-              setMessage(event.target.value);
+              setLastName(event.target.value);
             }}
           />
         </div>
         <div className="flex flex-row content-center gap-4 text-center">
-          <label className="row w-[50%] text-black">Name</label>
+          <label className="row w-[50%] text-black">Email</label>
           <input
-            type="text"
-            className="row rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 focus:outline-none"
-            placeholder="Your message..."
+            type="email"
+            className="row rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 text-yellow-100 focus:outline-none"
+            placeholder="Your Email..."
             minLength={2}
             maxLength={100}
-            value={message}
+            value={email}
             onChange={(event) => {
-              setMessage(event.target.value);
+              setEmail(event.target.value);
             }}
           />
         </div>
         <div className="flex flex-row content-center gap-4 text-center">
-          <label className="row w-[50%] text-black">Name</label>
+          <label className="row w-[50%] text-black">Phone Number</label>
           <input
-            type="text"
-            className="row rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 focus:outline-none"
-            placeholder="Your message..."
+            type="phone"
+            className="row rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 text-yellow-100 focus:outline-none"
+            placeholder="Your Phone Number..."
             minLength={2}
             maxLength={100}
-            value={message}
+            value={phoneNumber}
             onChange={(event) => {
-              setMessage(event.target.value);
+              setPhoneNumber(event.target.value);
             }}
           />
         </div>
         <div className="flex flex-row content-center gap-4 text-center">
-          <label className="row w-[50%] text-black">Name</label>
+          <label className="row w-[50%] text-black">What state do you live in the USA?</label>
+          <select
+            value={stateLiveIn}
+            onChange={(event) => {
+              setStateLiveIn(event.target.value);
+            }}
+            className="row rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 text-yellow-100 focus:outline-none"
+          >
+            <option value="AL">Alabama</option>
+            <option value="AK">Alaska</option>
+            <option value="AZ">Arizona</option>
+            <option value="AR">Arkansas</option>
+            <option value="CA">California</option>
+            <option value="CO">Colorado</option>
+            <option value="CT">Connecticut</option>
+            <option value="DE">Delaware</option>
+            <option value="DC">District Of Columbia</option>
+            <option value="FL">Florida</option>
+            <option value="GA">Georgia</option>
+            <option value="HI">Hawaii</option>
+            <option value="ID">Idaho</option>
+            <option value="IL">Illinois</option>
+            <option value="IN">Indiana</option>
+            <option value="IA">Iowa</option>
+            <option value="KS">Kansas</option>
+            <option value="KY">Kentucky</option>
+            <option value="LA">Louisiana</option>
+            <option value="ME">Maine</option>
+            <option value="MD">Maryland</option>
+            <option value="MA">Massachusetts</option>
+            <option value="MI">Michigan</option>
+            <option value="MN">Minnesota</option>
+            <option value="MS">Mississippi</option>
+            <option value="MO">Missouri</option>
+            <option value="MT">Montana</option>
+            <option value="NE">Nebraska</option>
+            <option value="NV">Nevada</option>
+            <option value="NH">New Hampshire</option>
+            <option value="NJ">New Jersey</option>
+            <option value="NM">New Mexico</option>
+            <option value="NY">New York</option>
+            <option value="NC">North Carolina</option>
+            <option value="ND">North Dakota</option>
+            <option value="OH">Ohio</option>
+            <option value="OK">Oklahoma</option>
+            <option value="OR">Oregon</option>
+            <option value="PA">Pennsylvania</option>
+            <option value="RI">Rhode Island</option>
+            <option value="SC">South Carolina</option>
+            <option value="SD">South Dakota</option>
+            <option value="TN">Tennessee</option>
+            <option value="TX">Texas</option>
+            <option value="UT">Utah</option>
+            <option value="VT">Vermont</option>
+            <option value="VA">Virginia</option>
+            <option value="WA">Washington</option>
+            <option value="WV">West Virginia</option>
+            <option value="WI">Wisconsin</option>
+            <option value="WY">Wyoming</option>
+          </select>
+        </div>
+        <div className="flex flex-row content-center gap-4 text-center">
+          <label className="row min-w-[50%] text-black">What kind of case do you have?</label>
+          <div className="flex items-center gap-2 text-center align-middle">
+            <button
+              className="flex items-center gap-2 text-center align-middle"
+              onClick={() => {
+                setTypeOfCase("Auto Accident");
+              }}
+            >
+              <input // change this for radio buttons options: Auto Accident - Workers' Compensation - Slip and Fall (Injury on Others' Property) - Wrongful Termination (Employment)
+                type="radio"
+                className="row rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 text-yellow-100 focus:outline-none"
+                minLength={2}
+                maxLength={100}
+                checked={typeOfCase === "Auto Accident"}
+              />
+              <label className="row w-[50%] text-black">Auto Accident</label>
+            </button>
+            <button
+              className="flex items-center gap-2 text-center align-middle"
+              onClick={() => {
+                setTypeOfCase("Worker's Compensation");
+              }}
+            >
+              <input
+                type="radio"
+                className="row rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 text-yellow-100 focus:outline-none"
+                minLength={2}
+                maxLength={100}
+                checked={typeOfCase === "Worker's Compensation"}
+              />
+              <label className="row w-[50%] text-black">{"Worker's Compensation"}</label>
+            </button>
+            <button
+              className="flex items-center gap-2 text-center align-middle"
+              onClick={() => {
+                setTypeOfCase("Slip and Fall (Injury on Other's Property)");
+              }}
+            >
+              <input
+                type="radio"
+                className="row rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 text-yellow-100 focus:outline-none"
+                minLength={2}
+                maxLength={100}
+                checked={typeOfCase === "Slip and Fall (Injury on Other's Property)"}
+              />
+              <label className="row w-[50%] text-black">
+                {"Slip and Fall (Injury on Other's Property)"}
+              </label>
+            </button>
+            <button
+              className="flex items-center gap-2 text-center align-middle"
+              onClick={() => {
+                setTypeOfCase("Wrongful Termination (Employment)");
+              }}
+            >
+              <input
+                type="radio"
+                className="row rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 text-yellow-100 focus:outline-none"
+                minLength={2}
+                maxLength={100}
+                checked={typeOfCase === "Wrongful Termination (Employment)"}
+              />
+              <label className="row w-[50%] text-black">Wrongful Termination (Employment)</label>
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-row content-center gap-4 text-center">
+          <label className="row w-[50%] text-black">
+            Are you currently working with an attorney and have a case pending?
+          </label>
+          <div className="flex gap-4">
+            <input // checkbox options: Yes or No
+              type="checkbox"
+              className="row rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 focus:outline-none"
+              checked={alreadyWorkingWithAttorney === "yes"}
+              onChange={() => {
+                setAlreadyWorkingWithAttorney("yes");
+              }}
+            />
+            <label className="row w-[50%] text-black">Yes</label>
+            <input
+              type="checkbox"
+              className="row rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 focus:outline-none"
+              checked={alreadyWorkingWithAttorney === "no"}
+              onChange={() => {
+                setAlreadyWorkingWithAttorney("no");
+              }}
+            />
+            <label className="row w-[50%] text-black">No</label>
+          </div>
+        </div>
+        <div className="flex flex-row content-center gap-4 text-center">
+          <label className="row w-[50%] text-black">Law Firm Name</label>
           <input
             type="text"
-            className="row rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 focus:outline-none"
-            placeholder="Your message..."
+            className="row rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 text-yellow-100 focus:outline-none"
+            placeholder="Your Law Firm Name..."
             minLength={2}
             maxLength={100}
-            value={message}
+            value={lawFirmName}
             onChange={(event) => {
-              setMessage(event.target.value);
+              setLawFirmName(event.target.value);
             }}
           />
         </div>
         <div className="flex flex-row content-center gap-4 text-center">
-          <label className="row w-[50%] text-black">Name</label>
+          <label className="row w-[50%] text-black">Lawyer Name</label>
           <input
             type="text"
-            className="row rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 focus:outline-none"
-            placeholder="Your message..."
+            className="row rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 text-yellow-100 focus:outline-none"
+            placeholder="Lawyer Name..."
             minLength={2}
             maxLength={100}
-            value={message}
+            value={lawyerName}
             onChange={(event) => {
-              setMessage(event.target.value);
+              setLawyerName(event.target.value);
             }}
           />
         </div>
         <div className="flex flex-row content-center gap-4 text-center">
-          <label className="row w-[50%] text-black">Name</label>
+          <label className="row w-[50%] text-black">Expected Settlement Amount in USD</label>
           <input
-            type="text"
-            className="row rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 focus:outline-none"
-            placeholder="Your message..."
+            type="number"
+            className="row rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 text-yellow-100 focus:outline-none"
+            placeholder="Expected Settlement Amount in USD..."
             minLength={2}
             maxLength={100}
-            value={message}
+            value={expectedSettlementAmount}
             onChange={(event) => {
-              setMessage(event.target.value);
+              setExpectedSettlementAmount(event.target.value);
             }}
           />
         </div>
--- apps/jurisfund/src/server/api/root.ts
+++ apps/jurisfund/src/server/api/root.ts
@@ -1,4 +1,5 @@
-import { exampleRouter } from "@/server/api/routers/example";
+import { applicationsRouter } from "@/server/api/routers/applications";
+// import { exampleRouter } from "@/server/api/routers/example";
 import { createTRPCRouter } from "@/server/api/trpc";
 
 /**
@@ -7,7 +8,8 @@ import { createTRPCRouter } from "@/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  // example: exampleRouter,
+  applications: applicationsRouter,
 });
 
 // export type definition of API
