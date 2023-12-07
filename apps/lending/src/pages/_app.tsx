c4e28befd15cca29df81f56ddbb702576591ff62
Marcos Iglesias <markkos89@gmail.com>
2023-12-07 11:57:44 -0300

    feat: cleaned up thirdweb integration

--- apps/lending/src/pages/_app.tsx
+++ apps/lending/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles.css";
 
-import { Sepolia } from "@thirdweb-dev/chains";
-import { embeddedWallet, smartWallet, ThirdwebProvider } from "@thirdweb-dev/react";
+// import { Sepolia } from "@thirdweb-dev/chains";
+// import { embeddedWallet, smartWallet, ThirdwebProvider } from "@thirdweb-dev/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 // import type { Session } from "next-auth";
@@ -9,15 +9,15 @@ import Head from "next/head";
 import { ThemeProvider } from "next-themes";
 
 import { Layout } from "@/components/Layout";
-import { env } from "@/env.mjs";
+// import { env } from "@/env.mjs";
 import { api } from "@/utils/api";
 
 export const factoryAddress = "0x76b7322f20a13aec6afc416222f2ce8ea23f6727";
 
-const smartWalletOptions = {
-  factoryAddress,
-  gasless: true,
-};
+// const smartWalletOptions = {
+//   factoryAddress,
+//   gasless: true,
+// };
 
 function MyApp(
   {
@@ -27,8 +27,7 @@ function MyApp(
 ) {
   return (
     <ThemeProvider attribute="class">
-      {/* <SessionProvider session={session}> */}
-      <ThirdwebProvider
+      {/* <ThirdwebProvider
         clientId={env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
         activeChain={Sepolia}
         supportedWallets={[
@@ -41,23 +40,22 @@ function MyApp(
             }),
             smartWalletOptions,
           ),
-        ]}
+        ]} 
         // authConfig={{
         //   domain: env.NEXT_PUBLIC_NEXTAUTH_URL,
         //   authUrl: "/api/auth",
         // }}
-      >
-        <Head>
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
-          />
-        </Head>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThirdwebProvider>
-      {/* </SessionProvider> */}
+      // > */}
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      {/* </ThirdwebProvider> */}
     </ThemeProvider>
   );
 }
